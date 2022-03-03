068be2df2cfffe88e1ca4ed265bf7c044a33df16
Ani-21 <kvikviniya.am@edu.spbstu.ru>
2022-03-03 15:15:26 +0300

    fixed/adaptive_style

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-'use strict'
+"use strict";
 
-function getDataInfo()
+const btnScrollTop = document.getElementById("btnTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnScrollTop.style.display = "block";
+  } else {
+    btnScrollTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
